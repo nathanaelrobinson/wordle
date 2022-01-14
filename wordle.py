ee7af2b59e292b613b9e7d1a3553497ec328451f
Nate Robinson <Nate@flashframe.io>
2022-01-14 00:14:40 -0800

    working with learning algo

--- wordle.py
+++ wordle.py
@@ -81,7 +81,7 @@ def eval_guess(word, target):
     for w, t in zip(word, target):
         if w == t:
             value = 'green'
-        elif any(w == i for i in target):
+        elif w in target:
             value = 'yellow'
         else:
             value = 'black'
@@ -102,28 +102,51 @@ def get_random_word():
     return random.choice(options)
 
 class WordleSolver:
-    def __init__(self):
+    def __init__(self, optional_weights=None):
         self.clues_array = np.zeros((26,5), dtype=int)
         self.options_array = np.ones((26,5), dtype=int)
-        # initialize weights array, this is the intelligent part of the program
-        # and will be updated by our ML model
-        self.weights = np.full((26, 5), 0.5)
         self.guesses_matrix = []
         self.guesses_text = []
         self.results = []
         self.options = np.load('eligible_words.npy')
+        # shuffle our list so it's not alphabetical
+        random.shuffle(self.options)
         self.score = 0
+        self.solved = False
+
+        # initialize weights array, this is the intelligent part of the program
+        # and will be updated by our ML model
+        if optional_weights is not None:
+            self.weights = optional_weights
+        else:
+            self.randomize_weights()
+            # self.weights = np.load('base_weights.npy')
 
     def select_best_guess(self):
         # if we know everything -- use that
         truth_array = np.array([1, 1, 1, 1, 1])
         if np.array_equal(sum(self.clues_array * self.options_array), truth_array):
-            guess_matrix = self.clues_array * self.options_array
-        # use a random function to retrieve guess for now
-        else:
-            random_int = random.randrange(len(self.options))
-            guess_matrix = self.options[random_int]
-        return guess_matrix
+            best_guess = self.clues_array * self.options_array
+            return best_guess
+
+        updated_weights = self.weights * self.options_array
+        best_val = 0
+        for option in self.options:
+            # trying to reduce computation time, if we've already guessed, then pass
+            feasibility = sum(sum(self.options_array * option))
+            option_already_guessed = matrix_to_text(option) in self.guesses_text
+            value = sum(sum(option * updated_weights))
+            if option_already_guessed:
+                    continue
+            # otherwise determine if this could even be an answer
+            # if not pass
+            elif feasibility ==  3:
+                    continue
+            # finally determine it's weighted match value for a guess
+            elif value > best_val:
+                best_guess = option
+                best_val = value
+        return best_guess
     
     def guess(self):
         # need a function to select our best guess
@@ -135,10 +158,10 @@ class WordleSolver:
         return guess_text
 
     def update_priors(self, guess_text, guess_result):
-        for i, letter, result in zip(range(0,6), guess_text, guess_result):
+        for i, letter, result in zip(np.arange(0,6, dtype=int), guess_text, guess_result):
             if result == 'green':
                 # first zero out all old yellow options
-                self.clues_array[LETTERS[letter], :] = 0
+                # self.clues_array[LETTERS[letter], :] = 0
                 # then mark known values as green
                 self.clues_array[LETTERS[letter], i] = 1
                 # finally zero out the options array for this position
@@ -148,6 +171,7 @@ class WordleSolver:
                 # we simply set this value to existing all rows bc it may duplicate
                 # and we'll just exclude it from it's current location
                 self.clues_array[LETTERS[letter], :] = 1
+                self.clues_array[LETTERS[letter], i] = 0
                 # we can still update the options array
                 self.options_array[LETTERS[letter], i] = 0
             elif result == 'black':
@@ -160,19 +184,89 @@ class WordleSolver:
         total = len(self.guesses_text)
         for guess_num, guess_word, result in zip(range(1, len(self.guesses_text)+1), self.guesses_text, self.results):
             print('%s %s %s/%s' %(guess_word, result_to_emojis(result), guess_num, total))
+        return
 
     def guess_and_check(self, target):
         guess = self.guess()
         result = eval_guess(guess, target)
         self.results.append(result)
         self.update_priors(guess, result)
-        self.score = score_guess(result)
+        self.score += score_guess(result)
+        if score_guess(result) == 500:
+            self.solved = True
         return
 
-    def run_sim(self, target):
-        while self.score < 500:
+    def assess_performance(self):
+        return len(self.guesses_text)
+   
+    def solve(self, target):
+        while not self.solved:
             self.guess_and_check(target)
-        self.print_results()
-        return
+        # self.print_results()
+        return 
+    
     def reset(self):
-        self.__init__()
+        self.__init__(self.weights)
+        return
+    
+    def randomize_weights(self):
+        self.weights = np.random.rand(26,5)
+        return
+
+def initiate_starting_population(size):
+    population = []
+    for i in range(size):
+        a = WordleSolver()
+        population.append(a)
+    return population
+
+def simulate_wordles_n(population, n):
+    results = []
+    for a in population:
+        scores = []
+        for n in range(n):
+            target = get_random_word()
+            a.solve(target)
+            scores.append(a.assess_performance())
+            a.reset()
+        result = {
+                'weights' : a.weights,
+                'avg_score' : sum(scores)/len(scores)
+        }
+        results.append(result)
+    results_sorted = sorted(results, key = lambda i: i['avg_score'],reverse=True)
+    return results_sorted
+
+def make_next_generation(results, alpha):
+    population_size = len(results)
+    # select our top two performing from the population
+    specimen_a = results[0]['weights']
+    specimen_b = results[1]['weights']
+    next_generation = []
+    for i in range(population_size):
+        # first randomly generate a weights matrix that is made up of A/B
+        # will contribute the first n random rows, b the last (26-n)
+        mix_factor = random.choice(np.arange(1,25, dtype=int))
+        part_a = specimen_a[:mix_factor, :]
+        part_b = specimen_a[mix_factor:, :]
+        child = np.concatenate((part_a, part_b), axis=0)
+        for i in range(26):
+            for j in range(5):
+                mutation = random.random()
+                if mutation > alpha:
+                    new_value = random.random()
+                    child[i,j] = new_value
+        a = WordleSolver(child)
+        next_generation.append(a)
+    return next_generation
+    
+def genetic_simulation(pop_size, mutation_alpha, generations):
+    pop = initiate_starting_population(pop_size)
+    for i in range(generations):
+        results = simulate_wordles_n(pop, 10)
+        # create new population from fittest
+        new_pop = make_next_generation(results, mutation_alpha)
+        pop = new_pop
+        print('Generation %s: Best Solver Score %s' %(i, results[0]['avg_score']))
+    return WordleSolver(results[0]['weights'])
+
